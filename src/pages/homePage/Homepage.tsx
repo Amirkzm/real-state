78437828f6536f2df607e21f65ee4cf65d329c01
Amir <kazemi.amir68@gmail.com>
2024-07-23 09:31:42 +0200

    feat: Add react-countup library for animated number display

--- src/pages/homePage/Homepage.tsx
+++ src/pages/homePage/Homepage.tsx
@@ -1,7 +1,10 @@
 import { SearchBar } from "../../components/searchBar";
+import CountUp from "react-countup";
 import "./homePage.scss";
+import { useState } from "react";
 
 const Homepage = () => {
+  const [showPlus, setShowPlus] = useState<boolean>(false);
   return (
     <div className="homepage">
       <div className="textContainer">
@@ -16,15 +19,28 @@ const Homepage = () => {
 
           <div className="boxes">
             <div className="box">
-              <h1>16+</h1>
+              <h1 className="number">
+                <CountUp start={0} end={16} duration={1} />
+                <div className="plusSign">+</div>
+              </h1>
               <h2>Years of Experience</h2>
             </div>
             <div className="box">
-              <h1>200</h1>
+              <h1 className="number">
+                <CountUp start={500} end={180} duration={1.5} />
+              </h1>
               <h2>Award Gained</h2>
             </div>
             <div className="box">
-              <h1>2000+</h1>
+              <h1 className="number">
+                <CountUp
+                  start={500}
+                  end={2000}
+                  duration={2}
+                  onEnd={() => setShowPlus(true)}
+                />
+                <div className="plusSign">+</div>
+              </h1>
               <h2>Property Ready</h2>
             </div>
           </div>
