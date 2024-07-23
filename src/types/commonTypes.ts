0b46b3c3b4f00ca89d90ed36b1e719e0f3550b0c
Amir <kazemi.amir68@gmail.com>
2024-07-23 09:36:54 +0200

    refactor: Add SingleLocationType and ChatMessageType

--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -1,4 +1,4 @@
-export type locationType = {
+export type ListLocationType = {
   id: number;
   title: string;
   img: string;
@@ -8,4 +8,28 @@ export type locationType = {
   address: string;
   latitude: number;
   longitude: number;
+};
+
+export type SingleLocationType = {
+  id: number;
+  title: string;
+  price: number;
+  images: string[];
+  bedRooms: number;
+  bathroom: number;
+  size: number;
+  latitude: number;
+  longitude: number;
+  city: string;
+  address: string;
+  school: string;
+  bus: string;
+  restaurant: string;
+  description: string;
+};
+
+export type ChatMessageType = {
+  message: string;
+  fromMe?: boolean;
+  time: string;
 };
